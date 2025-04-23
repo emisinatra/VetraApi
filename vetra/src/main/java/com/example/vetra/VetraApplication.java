326bd93506ab22c34e13eb9f3bc882d39db4fac1
Tonga <gaston.astudillo@gmail.com>
2025-04-23 08:42:39 -0300

    inicializacion de la api
    creacion del proyecto, 4 carpetas necesarias, etc

---
+++ vetra/src/main/java/com/example/vetra/VetraApplication.java
@@ -0,0 +1,13 @@
+package com.example.vetra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetraApplication.class, args);
+    }
+
+}
---
+++ vetra/src/test/java/com/example/vetra/VetraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vetra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
